45344e53c81e72b4032e64df3ff48ae263422612
Srujan KM <srujankm12@gmail.com>
2024-11-16 12:23:22 +0530

    machines added

--- src/lib/drawer.svelte
+++ src/lib/drawer.svelte
@@ -1,45 +1,38 @@
 <script>
-    import { goto } from "$app/navigation";
+   export let isDrawerOpen = false;
 
-    export let isDrawerOpen = false;
-
-    function toggleDrawer() {
+function toggleDrawer() {
         isDrawerOpen = !isDrawerOpen;
     }
 </script>
 
+
 {#if isDrawerOpen}
-    <!-- Background overlay -->
+
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <!-- svelte-ignore a11y_no_static_element_interactions -->
     <div
         class="fixed inset-0 bg-black bg-opacity-50 z-40"
         on:click={toggleDrawer} 
     ></div>
 
-    <!-- Drawer container -->
     <div
         class="fixed inset-y-0 left-0 w-60 bg-white shadow-xl z-50 transform transition-transform duration-300 ease-in-out rounded-r-2xl"
     >
-        <div class="px-4 flex flex-col h-full">
-            <div class="flex flex-col items-center mb-4 mt-5">
-                <i class="fa-solid fa-user-circle text-8xl text-blue-400"></i>
-                <span class="mt-3 text-lg font-semibold text-black">
-                    No Email Available
-                </span>
-                <hr class="w-full mt-2 border-gray-300" />
-            </div>
+      
 
             <ul class="flex flex-col flex-grow space-y-2 py-4">
                 <li>
                     <button
-                        class="flex items-center text-lg text-black text-left rounded-lg hover:text-white hover:bg-blue-400 duration-300 py-4 px-5 font-semibold w-full"
-                        on:click={() => {goto("/users"); isDrawerOpen = false;}}
+                        class="flex items-center text-lg text-black text-left rounded-lg hover:text-white hover:bg-black duration-300 py-4 px-5 font-semibold w-full"
+                        on:click={() => {goto("/machines");isDrawerOpen=false;}}
                     >
                         <i class="fa-solid fa-home mr-3"></i> Home
                     </button>
                 </li>
                 <li>
                     <button
-                        class="flex items-center text-lg text-black text-left rounded-lg hover:text-white hover:bg-blue-400 duration-300 py-4 px-5 font-semibold w-full"
+                        class="flex items-center text-lg text-black text-left rounded-lg hover:text-white hover:bg-black duration-300 py-4 px-5 font-semibold w-full"
                         on:click={() => goto("/giveaccess")}
                     >
                         <i class="fa fa-users mr-3"></i> Access
@@ -47,8 +40,8 @@
                 </li>
                 <li>
                     <button
-                        class="flex items-center text-lg text-black text-left rounded-lg hover:text-white hover:bg-blue-400 duration-300 py-4 px-5 font-semibold w-full"
-                        on:click={() => {goto("/settings"); isDrawerOpen = false;}}
+                        class="flex items-center text-lg text-black text-left rounded-lg hover:text-white hover:bg-black duration-300 py-4 px-5 font-semibold w-full"
+                        on:click={() => {goto("/settings");isDrawerOpen=false;}}
                     >
                         <i class="fa-solid fa-cog mr-3"></i> Settings
                     </button>
@@ -57,15 +50,17 @@
 
             <div class="mt-auto pb-4">
                 <button
-                    class="flex items-center text-lg text-black text-left rounded-lg hover:text-white hover:bg-blue-400 duration-300 py-4 px-5 font-semibold w-full"
+                    class="flex items-center text-lg text-black text-left rounded-lg hover:text-white hover:bg-black    duration-300 py-4 px-5 font-semibold w-full"
                     on:click={() => goto("/login")}
                 >
                     <i class="fa-solid fa-sign-out-alt mr-3"></i> Logout
                 </button>
             </div>
         </div>
-    </div>
+
 {/if}
 
-<!-- Drawer toggle button -->
-
+<!-- svelte-ignore a11y_consider_explicit_label -->
+<button on:click={toggleDrawer} class="fixed bottom-4 right-4 p-4 ">
+   
+</button>
